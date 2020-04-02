ae2caa31a8965975a099c67e37657c09624cc142
ManuFZJ <manuel.bayer@hhu.de>
2020-04-02 08:25:28 +0200

    reduced the spaghettiness of the code, replaced manual checks with functions, added h5d save and load

--- code/utility_functions.py
+++ code/utility_functions.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import re
 import os.path
 import time
+import random
 import subprocess
 
 import numpy as np
@@ -12,13 +13,72 @@ import nilearn as nil
 from numpy.testing.decorators import skipif
 import mvpa2.suite as mvpa
 
-
 # Get files
 def get_files(dataset_dir, rglob_string):
     files = [str(Path("/").joinpath((Path(file).relative_to("/"))))
              for file in dataset_dir.rglob(rglob_string)]
     files = np.sort(files).tolist()
     return files
+
+# Check if file/dir exists
+def check_exi(path):
+    path = Path(path)
+    if path.exists():
+        output = True
+    else:
+        output = False
+    return output
+
+# Check for file availability
+def check_file_completeness(file_list):
+    list_element = random.choice(file_list)
+    if "ao" in list_element and len(file_list) == 112:
+        output = True
+    elif "av" in list_element and len(file_list) == 120:
+        output = True
+    elif "events" in list_element and len(file_list) == 8:
+        output = True
+    elif "warp" in list_element and len(file_list) == 15:
+        output = True
+    else:
+        output = False
+    return output
+
+# Check if all components (directories, files, file_lists are complete)
+def check_all_components(path_list, file_lists):
+    path_exi_list = [
+        check_exi(element)
+        for element in path_list
+    ]
+    prob_paths_idx = [
+        index
+        for index, element in enumerate(path_exi_list)
+        if element == False
+    ]
+    path_output = "All directories seem to be properly set up"
+    if len(prob_paths_idx) != 0:
+        path_output = [
+            "This directory/file does not exist: {}".format(prob_ele)
+            for prob_ele in path_list
+            if path_list.index(prob_ele) in prob_paths_idx
+        ]
+    file_comp_list = [
+        check_file_completeness(file_list)
+        for file_list in file_lists
+    ]
+    prob_lists_idx = [
+        index
+        for index, element in enumerate(file_comp_list)
+        if element == False
+    ]
+    lists_output = "All file lists seem to complete"
+    if len(prob_lists_idx) != 0:
+        lists_output = [
+            "This list is incomplete: {}".format(prob_ele[0])
+            for prob_ele in file_lists
+            if file_lists.index(prob_ele) in prob_lists_idx
+        ]
+    return path_output, lists_output
 
 # Get participant + run number + stimulus type
 def find_participant_info(file_string):
@@ -73,17 +133,15 @@ def get_adjusted_mask(ori_mask, ref_space):
 # Preprocess individual dataset
 def preprocessing(ds_p, ref_space, warp_files, mask_p, detrending, use_zscore, use_events,
                   anno_dir, use_glm_estimates, targets, event_offset, event_dur):
-   
     vp_num_str = ds_p[(ds_p.find("sub")+4):(ds_p.find("sub")+6)]
     warp_file = [file for file in warp_files if file.find(vp_num_str) != -1][0]
-    
-    part_info = [ds_p[(ds_p.find("sub")+4):(ds_p.find("sub")+6)], 
-                        ds_p[(ds_p.find("task")+5):(ds_p.find("task")+7)],
-                        ds_p[(ds_p.find("run")+4):(ds_p.find("run")+5)]
-                        ]
-    temp_file = str((Path.cwd().parents[0]).joinpath("data", "tmp", 
-                            "sub-{}_{}-movie_run-{}_warped_file.nii.gz".format(part_info[0],
-                            part_info[1], part_info[2])))
+    part_info = [ds_p[(ds_p.find("sub")+4):(ds_p.find("sub")+6)],
+                 ds_p[(ds_p.find("task")+5):(ds_p.find("task")+7)],
+                 ds_p[(ds_p.find("run")+4):(ds_p.find("run")+5)]
+                 ]
+    temp_file_add = "sub-{}_{}-movie_run-{}_warped_file.nii.gz".format(part_info[0],
+                                                                       part_info[1], part_info[2])
+    temp_file = str((Path.cwd().parents[0]).joinpath("data", "tmp", temp_file_add))
     warped_ds = warp_image(ds_p, ref_space, warp_file, temp_file)
 
     while not os.path.exists(warped_ds):
@@ -95,7 +153,7 @@ def preprocessing(ds_p, ref_space, warp_files, mask_p, detrending, use_zscore, u
             ds = mvpa.fmri_dataset(samples=warped_ds, mask=mask)
         else:
             ds = mvpa.fmri_dataset(samples=warped_ds)
-        
+
     part_info = find_participant_info(ds_p)
     ds.sa['participant'] = [part_info[0] * ds.shape[0]]
     ds.sa['chunks'] = [part_info[1] * ds.shape[0]]
@@ -105,15 +163,16 @@ def preprocessing(ds_p, ref_space, warp_files, mask_p, detrending, use_zscore, u
     if use_zscore == True:
         mvpa.zscore(ds)
     if use_events == True:
-        events = create_event_dict(anno_dir, ds_p, part_info[2], 
-                        ['onset', 'duration', 'targets'], targets)
+        events = create_event_dict(anno_dir, ds_p, part_info[2],
+                                   ['onset', 'duration', 'targets'], targets)
         if use_glm_estimates == True:
             ds = mvpa.fit_event_hrf_model(ds, events, time_attr='time_coords',
-                        condition_attr='targets')
+                                          condition_attr='targets')
         else:
-            ds = mvpa.extract_boxcar_event_samples(ds, events=events,
-                        time_attr='time_coords', match='closest', event_offset=event_offset, 
-                        event_duration=event_dur,  eprefix='event', event_mapper=None)
+            ds = mvpa.extract_boxcar_event_samples(ds, events=events, time_attr='time_coords',
+                                                   match='closest', event_offset=event_offset,
+                                                   event_duration=event_dur, eprefix='event',
+                                                   event_mapper=None)
     return ds
 
 # Preprocess multiple datasets
@@ -121,13 +180,25 @@ def preprocess_datasets(dataset_list, ref_space, warp_files, mask, detrending, u
                         use_events, anno_dir, use_glm_estimates, targets, event_offset, event_dur):
     if isinstance(dataset_list, list):
         datasets = [preprocessing(ds_p, ref_space, warp_files, mask, detrending, use_zscore,
-                                  use_events, anno_dir, use_glm_estimates, targets,  event_offset,
+                                  use_events, anno_dir, use_glm_estimates, targets, event_offset,
                                   event_dur)
-                            for ds_p in dataset_list]
+                    for ds_p in dataset_list]
         ds = mvpa.vstack(datasets, a='drop_nonunique', fa='drop_nonunique')
     else:
         ds = preprocessing(dataset_list, ref_space, warp_files, mask, detrending,
-                                        use_zscore, use_events, anno_dir, use_glm_estimates,
-                                        targets, event_offset, event_dur)
+                           use_zscore, use_events, anno_dir, use_glm_estimates,
+                           targets, event_offset, event_dur)
     return ds
-    
+
+# Load or create and save ds
+def load_create_save_ds(ds_save_p, dataset_list, ref_space, warp_files, mask, detrending,
+                        use_zscore, use_events, anno_dir, use_glm_estimates, targets,
+                        event_offset, event_dur):
+    if ds_save_p.exists():
+        ds = mvpa.h5load(ds_save_p)
+    else:
+        ds = preprocess_datasets(dataset_list, ref_space, warp_files, mask, detrending,
+                                 use_zscore, use_events, anno_dir, use_glm_estimates, targets,
+                                 event_offset, event_dur)
+        mvpa.h5save(ds_save_p, ds)
+    return ds
